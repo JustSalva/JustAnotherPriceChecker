cce0cb41a4bcc84c4209185d022d93624a9a8d28
Matteo Salvadore <matteo.salvadore@gmail.com>
2019-08-28 10:28:59 +0200

    fixed amazon price checker
    when amazon price is not defined we take the lower customer's price

--- price_checker.py
+++ price_checker.py
@@ -16,6 +16,22 @@ def website_function_selector(website_name: str):
     return switch.get(website_name, default)
 
 
+def amazon_page_parser(web_page):
+    # read title and price from amazon page
+    page_content = BeautifulSoup(web_page.content, 'html.parser')
+
+    price = page_content.find(id='priceblock_ourprice')
+    if price is None:
+        price = page_content.find(id='a-color-price')
+    price = price.get_text()
+    title = page_content.find(id='productTitle').get_text()
+    title = title.strip()
+    price, currency = price.split()
+    price = price.replace(',', '.')
+    price = float(price)
+    return title, price, currency
+
+
 def check_amazon_price(url: str, required_price: float, action_to_perform: Callable[[dict], None], **kwargs: dict):
     module_logger.info('Performing get request...')
 
@@ -25,14 +41,7 @@ def check_amazon_price(url: str, required_price: float, action_to_perform: Calla
         module_logger.error(requestFailedException.message)
         return
 
-    # read title and price from amazon page
-    page_content = BeautifulSoup(web_page.content, 'html.parser')
-    price = page_content.find(id='priceblock_ourprice').get_text()
-    title = page_content.find(id='productTitle').get_text()
-    title = title.strip()
-    price, currency = price.split()
-    price = price.replace(',', '.')
-    price = float(price)
+    title, price, currency = amazon_page_parser(web_page)
 
     if price <= required_price:
         # set up values to be inserted in the notification
