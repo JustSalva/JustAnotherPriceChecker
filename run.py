fb1d915dd6bc8a44f251e39cbf33f1c1572710fe
Matteo Salvadore <matteo.salvadore@gmail.com>
2019-08-28 10:54:45 +0200

    wrongly raised exception

--- run.py
+++ run.py
@@ -27,7 +27,6 @@ if __name__ == '__main__':
     try:
         while True:
             read_json_config_file()
-            raise Exception
             for element_to_be_monitored in amazon_urls_to_be_monitored_list:
                 url = element_to_be_monitored['url']
                 required_price = element_to_be_monitored['required_price']
